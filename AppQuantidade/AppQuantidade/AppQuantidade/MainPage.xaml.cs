0d1f526656e2767bd264a9ad3a2573b070cad79f
Rafael Coutinho <coutinhor@knowfully.com>
2021-04-15 08:49:19 -0300

    Created the buttons and logic

--- AppQuantidade/AppQuantidade/AppQuantidade/MainPage.xaml.cs
+++ AppQuantidade/AppQuantidade/AppQuantidade/MainPage.xaml.cs
@@ -10,9 +10,26 @@ namespace AppQuantidade
 {
     public partial class MainPage : ContentPage
     {
+        int quantidade = 1;
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void Aumentar(object sender, EventArgs e)
+        {
+            quantidade++;
+            lblQuantidade.Text = quantidade.ToString();
+        }
+
+        private void Diminuir(object sender, EventArgs e)
+        {
+            if (quantidade != 0)
+            {
+                quantidade--;
+                lblQuantidade.Text = quantidade.ToString();
+            }
         }
     }
 }
